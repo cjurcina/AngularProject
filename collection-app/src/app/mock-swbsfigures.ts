bde7d1b649d885386b38ab5dfc9cb4b0e8a30ac5
Christopher Jurcina <cjurcina@yahoo.com>
2022-06-03 15:10:49 -0300

    ANGPROJ: Add list of figures
    - Added list fo figures to black series component.
    - Added new styling for component and list.

---
+++ collection-app/src/app/mock-swbsfigures.ts
@@ -0,0 +1,10 @@
+import { Figure } from "./figure";
+
+export const SWBSFIGURES: Figure[] = [
+    { name: 'Darth Maul', value: 30, line: 'Archive' },
+    { name: 'Captain Rex', value: 50, line: 'Main Line' },
+    { name: 'Boba Fett (Throne Room)', value: 30, line: 'Deluxe' },
+    { name: 'Bo-Katan Kryze', value: 30, line: 'Main Line' },
+    { name: 'Darth Nihilus', value: 60, line: 'Gaming Greats' },
+    { name: 'Clone Commando Boss', value: 30, line: 'Gaming Greats' }
+];
